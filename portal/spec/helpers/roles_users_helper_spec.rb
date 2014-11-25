5447c8157ce3c97879d7806432914dd2ffd49a5e
nagamohan <nagamohan2014@gmail.com>
2014-11-25 20:40:27 +0530

    new controller for roles users

---
+++ portal/spec/helpers/roles_users_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the RolesUsersHelper. For example:
+#
+# describe RolesUsersHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe RolesUsersHelper, :type => :helper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
